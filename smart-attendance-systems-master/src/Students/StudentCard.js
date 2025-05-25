bfde8d21cb2c1067652d75c8193c327aea527cb3
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-25 08:42:04 +0530

    updated

--- smart-attendance-systems-master/src/Students/StudentCard.js
+++ smart-attendance-systems-master/src/Students/StudentCard.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './Students.css';
+import React from "react";
+import "./Students.css";
 
 const StudentCard = ({ student }) => {
   return (
     <div className="student-card">
       <div className="avatar" />
-      <div className='photo'><img src={`https://finallyback-3.onrender.com/api/v1/admin/getalluser${student.avatar}`}alt='sartaj'/></div>
+      <div className="photo">
+        <img
+          src={`https://finallyback-3.onrender.com/${student.avatar}`}
+          alt="sartaj"
+        />
+      </div>
       <h3>{student.name}</h3>
+      <h3>{student.fullName}</h3>
       <p>ID: {student.rfid}</p>
       <p>{student.email}</p>
       <p>Branch : {student.branch}</p>
       <p>Batches : {student.batches}</p>
-      
     </div>
   );
 };
