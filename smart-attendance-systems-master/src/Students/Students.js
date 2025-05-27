af68da71b3a4e61622d773c36f278b021341c46e
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-27 15:57:33 +0530

    updated

--- smart-attendance-systems-master/src/Students/Students.js
+++ smart-attendance-systems-master/src/Students/Students.js
@@ -16,7 +16,7 @@ const App = () => {
   useEffect(() => {
     const fetchStudents = async () => {
       const data = await fetch(
-        "https://finallyback-4.onrender.com/api/v1/users/getalluser"
+        "https://finallyback-4.onrender.com/api/v1/admin/getalluser"
       );
       const result = await data.json();
 
