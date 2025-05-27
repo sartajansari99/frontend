120d0b03b35fa55e5372121e3741340496ff349f
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-27 16:06:03 +0530

    updated

--- smart-attendance-systems-master/src/Attendence_Log/Attendence_Log.js
+++ smart-attendance-systems-master/src/Attendence_Log/Attendence_Log.js
@@ -26,7 +26,7 @@ const AttendanceReport = () => {
       {attendanceData.map((entry, index) => (
         <div className="log-row" key={index}>
           <div className="photo-circle">
-            <img src={entry.avatar} alt="sartaj" />
+            <img src={`https://finallyback-4.onrender.com/${entry.avatar}`} alt="sartaj" />
           </div>
           <div className="log-info">
             <span>{entry.name}</span>
