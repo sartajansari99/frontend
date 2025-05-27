3bb2b57057a650ffab86ccf2092994b749f6c43b
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-27 15:27:13 +0530

    update

--- smart-attendance-systems-master/src/Registration_page/Registration_page.js
+++ smart-attendance-systems-master/src/Registration_page/Registration_page.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Register.css";
+import "./Registration_page.css";
 
 const Register = () => {
   const [formData, setFormData] = useState({
