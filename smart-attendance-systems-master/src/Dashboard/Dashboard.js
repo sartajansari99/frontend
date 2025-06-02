91fea76ef055b31c973f42943f69886f9c1d8974
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-02 17:53:13 +0530

    fs

--- smart-attendance-systems-master/src/Dashboard/Dashboard.js
+++ smart-attendance-systems-master/src/Dashboard/Dashboard.js
@@ -171,10 +171,7 @@ function LoginSignup() {
 
 function ClientPage() {
   return (
-    <>
       <h2 style={{ padding: "2rem" }}>Welcome Client</h2>
-      <h4>Working on this pages</h4>
-    </>
   );
 }
 
