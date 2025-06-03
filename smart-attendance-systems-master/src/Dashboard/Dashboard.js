16ca67e0ab9f993d0cacbaea97a4bef54f0a71b0
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-03 11:09:08 +0530

    updates

--- smart-attendance-systems-master/src/Dashboard/Dashboard.js
+++ smart-attendance-systems-master/src/Dashboard/Dashboard.js
@@ -53,7 +53,7 @@ function LoginSignup() {
         console.log(data);
 
         localStorage.setItem("token", data.data.accessToken);
-        localStorage.setItem("user", JSON.stringify(data.data.user));
+        localStorage.setItem("user", JSON.stringify(data.data.user.fullName));
         navigate(role === "Admin" ? "/admin/admin_dashboard" : "/client");
       } else {
         setError(data.message || "Invalid credentials");
