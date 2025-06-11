d187240e7b1091f12a58d4059dbcbf3662728103
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-11 21:52:23 +0530

    Fas

--- smart-attendance-systems-master/src/Admin_Dashboard/AdminDashboard.js
+++ smart-attendance-systems-master/src/Admin_Dashboard/AdminDashboard.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./AdminDashboard.css";
+import { useEffect } from "react";
 
 const AdminPanel = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    // ✅ Redirect if no access token
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      navigate("/");
+      return;
+    }
+  });
   return (
     <div className="admin-container">
       <h1 className="admin-title">ADMIN PANEL</h1>
       <div className="button-grid">
-        <button onClick={() => navigate("/admin/admin_dashboard/students")}>STUDENTS</button>
-        <button onClick={() => navigate("/admin/admin_dashboard/manage-subject")}>MANAGE SUBJECT</button>
-        <button onClick={() => navigate("/admin/admin_dashboard/attendance-report")}>ATTENDANCE REPORT</button>
-        <button onClick={() => navigate("/admin/admin_dashboard/attendance-log")}>ATTENDANCE LOG</button>
+        <button onClick={() => navigate("/admin/admin_dashboard/students")}>
+          STUDENTS
+        </button>
+        <button
+          onClick={() => navigate("/admin/admin_dashboard/manage-subject")}
+        >
+          MANAGE SUBJECT
+        </button>
+        <button
+          onClick={() => navigate("/admin/admin_dashboard/attendance-report")}
+        >
+          ATTENDANCE REPORT
+        </button>
+        <button
+          onClick={() => navigate("/admin/admin_dashboard/attendance-log")}
+        >
+          ATTENDANCE LOG
+        </button>
       </div>
     </div>
   );
